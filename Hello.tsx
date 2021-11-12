946f4715884dbad9cce012665221f52233881dae
suryasg165 <suryanarayana.pabbineedi@senecaglobal.com>
2021-11-12 12:59:35 +0530

    Initial commit

---
+++ Hello.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default ({ name }) => (
+  <>
+    <h1>Welcome Aspirant,</h1>
+    <section>
+      Demostrate your basic react skills by developing a simple application:
+      <ul>
+        <li>GET users from https://jsonplaceholder.typicode.com/users/</li>
+        <li>
+          Create component(s) that will take users as prop and render name &
+          email. The list should be sortable and searchable by name.
+        </li>
+        <li>
+          Each user item should have an action 'View Detail' which displays
+          address & company
+        </li>
+      </ul>
+    </section>
+  </>
+);
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-ejdu6z
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-ejdu6z)
